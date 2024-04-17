3d3914e32388dca013487a05478d608f1c96195c
jetrico <jetriconew@gmail.com>
2024-04-17 17:51:33 +0800

    Game prevent click-through

--- src/scenes/index.js
+++ src/scenes/index.js
@@ -2,6 +2,8 @@ import Phaser from 'phaser';
 
 class IndexScene extends Phaser.Scene {
   preload() {
+    this.input.topOnly = true;
+
     const { width, height } = this.game.canvas;
     const background = this.add.graphics();
     background.fillStyle(0xede5dd);
@@ -9,8 +11,8 @@ class IndexScene extends Phaser.Scene {
 
     // Google Fonts
     this.load.rexWebFont({
-      google: { families: ['Pacifico'] },
-      testString: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz 1234567890 !@#$%^&*()-_=+[{]}\\|;:\'",<.>/?',
+      google: { families: ['Pacifico', 'Zen Maru Gothic'] },
+      testString: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz 1234567890 !@#$%^&*()-_=+[{]}\\|;:\'",<.>/? 人類',
     });
   }
 
@@ -31,9 +33,40 @@ class IndexScene extends Phaser.Scene {
         .setOrigin(0, 0)
         .setInteractive({ useHandCursor: true })
         .on('pointerdown', () => {
+          this.pauseFilter.setVisible(true);
           this.game.vue.openProject({ key: project });
         });
+      this.add.text(410 + (i * 120), 540, project, {
+        fontFamily: 'Zen Maru Gothic',
+        fontStyle: 'bold',
+        fontSize: 18,
+        align: 'center',
+        fixedWidth: 100,
+        color: '#f0f0f0',
+        stroke: '#1a1a1a',
+        strokeThickness: 3,
+      });
     });
+
+    this.pauseFilter = this.add.container(0, 0, [
+      this.add.rectangle(0, 0, width, height, 0x000000, 0.85)
+        .setOrigin(0, 0)
+        .setInteractive({ useHandCursor: true })
+        .on('pointerdown', () => {
+          const projectOpen = Object.values(this.game.vue.show).reduce((c, v) => c || v, false);
+          if (!projectOpen) this.pauseFilter.setVisible(false);
+        }),
+      this.add.text(0, height * 0.75, 'Click to resume game', {
+        fontFamily: 'Zen Maru Gothic',
+        fontStyle: 'bold',
+        fontSize: 30,
+        align: 'center',
+        fixedWidth: width,
+        color: '#f0f0f0',
+        stroke: '#1a1a1a',
+        strokeThickness: 3,
+      }),
+    ]).setVisible(false);
   }
 }
 
