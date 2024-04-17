9c7f02cb243dbe359103df4594067b9bef8729d0
jetrico <jetriconew@gmail.com>
2024-04-17 15:24:29 +0800

    Add Rex plugins, Google Fonts

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,4 +1,11 @@
+import WebFontLoaderPlugin from 'phaser3-rex-plugins/plugins/webfontloader-plugin';
+
 export default {
   global: [
+    {
+      key: 'rexWebFontLoader',
+      plugin: WebFontLoaderPlugin,
+      start: true,
+    },
   ],
 };
--- src/scenes/index.js
+++ src/scenes/index.js
@@ -2,13 +2,29 @@ import Phaser from 'phaser';
 
 class IndexScene extends Phaser.Scene {
   preload() {
+    const { width, height } = this.game.canvas;
+    const background = this.add.graphics();
+    background.fillStyle(0xede5dd);
+    background.fillRect(0, 0, width, height);
+
+    // Google Fonts
+    this.load.rexWebFont({
+      google: { families: ['Pacifico'] },
+      testString: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz 1234567890 !@#$%^&*()-_=+[{]}\\|;:\'",<.>/?',
+    });
   }
 
   create() {
     const { width, height } = this.game.canvas;
-    const background = this.add.graphics();
-    background.fillStyle(0xfff7f9);
-    background.fillRect(0, 0, width, height);
+    this.add.text(0, height * 0.45, 'Happy Bithday Nanashi Mumei!', {
+      fontFamily: 'Pacifico',
+      fontSize: 50,
+      align: 'center',
+      fixedWidth: width,
+      color: '#964B00',
+      stroke: '#f3c698',
+      strokeThickness: 5,
+    });
   }
 }
 
