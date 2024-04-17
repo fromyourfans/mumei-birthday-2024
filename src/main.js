52eacc56f87697c103521629f29fd40cf1cd4c11
jetrico <jetriconew@gmail.com>
2024-04-17 15:06:49 +0800

    Cleanup unused components

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import './assets/main.css'
 import '@mdi/font/css/materialdesignicons.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
 
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
@@ -24,7 +22,5 @@ const vuetify = createVuetify({
 })
 
 const app = createApp(App)
-app.use(createPinia())
-app.use(router)
 app.use(vuetify)
 app.mount('#app')
