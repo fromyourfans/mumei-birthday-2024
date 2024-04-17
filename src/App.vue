ce3942da5783753d9cba8a515074e0c61549be65
jetrico <jetriconew@gmail.com>
2024-04-17 13:40:19 +0800

    Add Vuetify + Icons

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,8 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
+
+      <v-icon icon="mdi-home" />
     </div>
   </header>
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import './assets/main.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,9 +7,24 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: { mdi },
+  },
+})
+
+const app = createApp(App)
 app.use(createPinia())
 app.use(router)
-
+app.use(vuetify)
 app.mount('#app')
