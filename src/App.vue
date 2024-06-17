fd7c1129a87dd7399e221ec21206fd2aa454d301
jetrico <jetriconew@gmail.com>
2024-06-17 12:37:51 +0800

    Game scale mode fit to viewport

--- src/App.vue
+++ src/App.vue
@@ -71,6 +71,7 @@ export default {
       banner: false,
       backgroundColor: Phaser.Display.Color.HexStringToColor('#1a1a1a').color,
       scale: {
+        mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH,
         width: 1280,
         height: 720,
